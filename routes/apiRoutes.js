18d20b4e1e84622c774f403d4d8f637ff4f3a657
Christian Yonge <christianyonge93@gmail.com>
2020-02-05 14:17:36 -0600

    adding calculation in api routes for s and p over amount

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,6 +17,30 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/api/data', function(req, res){
+    var oneYearFromNow = new Date();
+    oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() - 1);
+    db.spFiveHundred.findAll({
+      where: {
+        price_date :{
+          [db.Sequelize.Op.gte]: oneYearFromNow
+        }
+      }
+    }).then(data => {
+      var amount = 200;
+      totalShares = 0
+      for(var i = 0; i < data.length; i++){
+        totalShares +=  amount/+data[i].open_price
+      }
+
+      var money = totalShares * +data[0].open_price
+      
+      console.log(money);
+      res.json(data)
+    
+    });
+  })
+
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
